9c947c7f865bc9a99dd5d09d5982d511a67bcdef
khwilo <khwilowatai@gmail.com>
2019-05-09 17:19:46 +0300

    Implement endpoint for signing in

--- src/main/java/com/khwilo/movieapi/controller/UserController.java
+++ src/main/java/com/khwilo/movieapi/controller/UserController.java
@@ -1,19 +1,27 @@
 package com.khwilo.movieapi.controller;
 
+import com.khwilo.movieapi.auth.JwtTokenProvider;
+import com.khwilo.movieapi.dao.UserRepository;
 import com.khwilo.movieapi.model.User;
+import com.khwilo.movieapi.model.UserLogin;
 import com.khwilo.movieapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/users")
     public ResponseEntity<Object> getAllUsers() {
@@ -25,10 +33,20 @@ public class UserController {
         return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
     }
 
-    @PostMapping("/users")
+    @PostMapping("/auth/register")
     public ResponseEntity<Object> saveUser(@RequestBody User user) {
         userService.save(user);
         return new ResponseEntity<>("You have registered successfully", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> loginUser(@RequestBody UserLogin userLogin) {
+        String userName = userLogin.getUserName();
+        User user = userRepository.findUserByUserName(userName);
+        if (user.getUserName() != userName) {
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity<>("You have successfully logged in", HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{id}")
