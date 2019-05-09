9e804698bfb944f434fc3724bb16d07b33f113a5
khwilo <khwilowatai@gmail.com>
2019-05-09 15:42:14 +0300

    Create the movie service

---
+++ src/main/java/com/khwilo/movieapi/service/MovieService.java
@@ -0,0 +1,41 @@
+package com.khwilo.movieapi.service;
+
+import com.khwilo.movieapi.dao.MovieRepository;
+import com.khwilo.movieapi.model.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired
+    MovieRepository movieRepository;
+
+    public Movie getMovieByTitle(String title) {
+        return movieRepository.findMovieByTitle(title);
+    }
+
+    public List<Movie> getAllMovies() {
+        List<Movie> movies = new ArrayList<>();
+        movieRepository.findAll().forEach(
+                movie -> movies.add(movie)
+        );
+        return movies;
+    }
+
+    public void save(Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    public void updateMovie(int id, Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    public void deleteMovie(int id) {
+        movieRepository.deleteById(id);
+    }
+
+
+}
