906cbdc7ef01c23ca4f0172b9b9237d4133f33b4
khwilo <khwilowatai@gmail.com>
2019-05-14 07:04:46 +0300

    Add methods to find users by username and email address

--- src/main/java/com/khwilo/movieapi/dao/UserRepository.java
+++ src/main/java/com/khwilo/movieapi/dao/UserRepository.java
@@ -3,6 +3,12 @@ package com.khwilo.movieapi.dao;
 import com.khwilo.movieapi.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
-    User findUserByUserName(String userName);
+    Optional<User> findUserByUserName(String userName);
+    Optional<User> findUserByEmailAddress(String emailAddress);
+    Optional<User> findUserByUserNameOrEmailAddress(String username, String emailAddress);
+    Boolean existsByUserName(String username);
+    Boolean existsByEmailAddress(String emailAddress);
 }
