ee40156fe36a938e94f7d9527138baf25881e20d
khwilo <khwilowatai@gmail.com>
2019-05-14 09:17:07 +0300

    Implement the logic to authenticate a user

--- src/main/java/com/khwilo/movieapi/security/JwtAuthenticationFilter.java
+++ src/main/java/com/khwilo/movieapi/security/JwtAuthenticationFilter.java
@@ -1,72 +1,60 @@
-package com.khwilo.movieapi.auth;
+package com.khwilo.movieapi.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khwilo.movieapi.constants.SecurityConstants;
-import com.khwilo.movieapi.model.User;
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
 
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
-    private AuthenticationManager authenticationManager;
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
 
-    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
-
-    @Override
-    public Authentication attemptAuthentication(
-            HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        try {
-            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
-            return authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            user.getUserName(),
-                            user.getPassword(),
-                            new ArrayList<>()
-                    )
-            );
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-    }
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
     @Override
-    protected void successfulAuthentication(
+    protected void doFilterInternal(
             HttpServletRequest request, HttpServletResponse response,
-            FilterChain filterChain, Authentication authentication) {
-//        User user = ((User) authentication.getPrincipal());
-//        byte[] signingKey = SecurityConstants.JWT_SECRET_KEY.getBytes();
-//        String token = Jwts.builder()
-//                .signWith(Keys.hmacShaKeyFor(signingKey), SignatureAlgorithm.HS256)
-//                .setHeaderParam("type", SecurityConstants.TOKEN_TYPE)
-//                .setIssuer(SecurityConstants.TOKEN_ISSUER)
-//                .setAudience(SecurityConstants.TOKEN_AUDIENCE)
-//                .setSubject(user.getUserName())
-//                .setExpiration(new Date(System.currentTimeMillis() + 864000000))
-//                .compact();
+            FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = getJwtFromRequest(request);
 
-        String token = jwtTokenProvider.generateToken(authentication);
-        response.addHeader(SecurityConstants.TOKEN_HEADER, SecurityConstants.TOKEN_PREFIX + token);
+            if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+                Long userId = jwtTokenProvider.getUserIdFromJwt(jwt);
+                UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails, null, userDetails.getAuthorities()
+                        );
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception ex) {
+            logger.error("Could not set user authentication in security context", ex);
+        }
+
+        filterChain.doFilter(request, response);
     }
 
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(SecurityConstants.TOKEN_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
