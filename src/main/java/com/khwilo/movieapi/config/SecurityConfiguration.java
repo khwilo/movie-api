255973e50ed690033cc294f0009838adb149c41e
khwilo <khwilowatai@gmail.com>
2019-05-14 12:13:58 +0300

    Implement the method to encode a user's password

--- src/main/java/com/khwilo/movieapi/config/SecurityConfiguration.java
+++ src/main/java/com/khwilo/movieapi/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.khwilo.movieapi.config;
 
+import com.khwilo.movieapi.security.CustomUserDetailsService;
 import com.khwilo.movieapi.security.JwtAuthenticationEntryPoint;
 import com.khwilo.movieapi.security.JwtAuthenticationFilter;
 import com.khwilo.movieapi.constants.SecurityConstants;
@@ -8,16 +9,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
     @Autowired
     JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
@@ -26,10 +33,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new JwtAuthenticationFilter();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
