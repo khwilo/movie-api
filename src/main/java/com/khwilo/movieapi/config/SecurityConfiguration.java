25b7b9ff6bca59b8bee97200e98a2bd5b8b83041
khwilo <khwilowatai@gmail.com>
2019-05-14 09:19:51 +0300

    Define the security configurations for the application

--- src/main/java/com/khwilo/movieapi/config/SecurityConfiguration.java
+++ src/main/java/com/khwilo/movieapi/config/SecurityConfiguration.java
@@ -1,46 +1,49 @@
-package com.khwilo.movieapi.configuration;
+package com.khwilo.movieapi.config;
 
-import com.khwilo.movieapi.auth.JWTAuthorizationFilter;
-import com.khwilo.movieapi.auth.JwtAuthenticationFilter;
+import com.khwilo.movieapi.security.JwtAuthenticationEntryPoint;
+import com.khwilo.movieapi.security.JwtAuthenticationFilter;
 import com.khwilo.movieapi.constants.SecurityConstants;
-import com.khwilo.movieapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                .antMatchers(
-                        SecurityConstants.AUTH_SIGNUP_URL,
-                        SecurityConstants.AUTH_LOGIN_URL,
-                        SecurityConstants.MOVIE_URL,
-                        SecurityConstants.MOVIE_URL_ID_PATH).permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers(SecurityConstants.ACTUATOR).permitAll()
+                .antMatchers(SecurityConstants.H2_CONSOLE).permitAll()
+                .antMatchers(HttpMethod.POST, SecurityConstants.AUTH_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
+                .csrf().ignoringAntMatchers(SecurityConstants.H2_CONSOLE, SecurityConstants.AUTH_URL)
+                .and()
+                .headers().frameOptions().sameOrigin();
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
