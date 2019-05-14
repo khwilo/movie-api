d8c0b49381909cd4b5bc119637e3417d57a3a8bd
khwilo <khwilowatai@gmail.com>
2019-05-14 10:48:44 +0300

    Remove unused methods to find a user by username / email

--- src/main/java/com/khwilo/movieapi/dao/UserRepository.java
+++ src/main/java/com/khwilo/movieapi/dao/UserRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByUserName(String userName);
-    Optional<User> findUserByEmailAddress(String emailAddress);
     Optional<User> findUserByUserNameOrEmailAddress(String username, String emailAddress);
     Boolean existsByUserName(String username);
     Boolean existsByEmailAddress(String emailAddress);
