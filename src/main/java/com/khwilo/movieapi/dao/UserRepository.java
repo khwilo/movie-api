605c283506674d4e2d12fff33fd41305173f8193
khwilo <khwilowatai@gmail.com>
2019-05-14 08:23:37 +0300

    Change the 'id' data type from 'Integer' to 'Long'

--- src/main/java/com/khwilo/movieapi/dao/UserRepository.java
+++ src/main/java/com/khwilo/movieapi/dao/UserRepository.java
@@ -1,11 +1,11 @@
 package com.khwilo.movieapi.dao;
 
 import com.khwilo.movieapi.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUserName(String userName);
     Optional<User> findUserByEmailAddress(String emailAddress);
     Optional<User> findUserByUserNameOrEmailAddress(String username, String emailAddress);
