2af19e010fa5b969127cd8e263ac5909b03bbe06
khwilo <khwilowatai@gmail.com>
2019-05-27 21:48:26 +0300

    Create the movie request payload

---
+++ src/main/java/com/khwilo/movieapi/payload/MovieRequest.java
@@ -0,0 +1,27 @@
+package com.khwilo.movieapi.payload;
+
+import javax.validation.constraints.NotBlank;
+
+public class MovieRequest {
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
