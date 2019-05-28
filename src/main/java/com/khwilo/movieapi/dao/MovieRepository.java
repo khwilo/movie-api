4076ae5e1ac7ec51e268004b2f6f1b9dcc5b2396
khwilo <khwilowatai@gmail.com>
2019-05-28 09:35:41 +0300

    Remove the method to find a movie by its ID and the user's ID

--- src/main/java/com/khwilo/movieapi/dao/MovieRepository.java
+++ src/main/java/com/khwilo/movieapi/dao/MovieRepository.java
@@ -3,9 +3,6 @@ package com.khwilo.movieapi.dao;
 import com.khwilo.movieapi.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     Movie findMovieByTitle(String title);
-    Optional<Movie> findByIdAndUserId(Long id, Long userId);
 }
